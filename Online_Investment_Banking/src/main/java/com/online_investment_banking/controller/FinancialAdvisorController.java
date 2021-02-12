4b40a84fb0080c02080b9af6650376f48ee49a02
priyamanoj <priyamanoj@192.168.1.193>
2021-02-11 18:11:14 -0600

    pushing controller and service changes

--- Online_Investment_Banking/src/main/java/com/online_investment_banking/controller/FinancialAdvisorController.java
+++ Online_Investment_Banking/src/main/java/com/online_investment_banking/controller/FinancialAdvisorController.java
@@ -3,13 +3,18 @@ package com.online_investment_banking.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.online_investment_banking.model.FinancialAdvisor;
 import com.online_investment_banking.service.FinancialAdvisorService;
+
 
 @CrossOrigin
 @RestController
@@ -20,10 +25,27 @@ public class FinancialAdvisorController {
 	private FinancialAdvisorService financialAdvisorService;
 	
 	@GetMapping("/getFAlist")
-	public List<FinancialAdvisor> listFinancialadvisors() {
+	public List<FinancialAdvisor> getAllFinacialAdvisors() {
 		
 		return financialAdvisorService.getAllFinacialAdvisors();
 		
 	}
 
+	@PostMapping("/createFA")
+	public FinancialAdvisor createFinancialAdvisor(@RequestBody FinancialAdvisor financialAdvisor)
+	{
+		return financialAdvisorService.createFinancialAdvisor(financialAdvisor);
+	}
+	
+	@GetMapping("/getFA/{fid}")
+    public ResponseEntity<FinancialAdvisor> getFinancialAdvisor(@PathVariable long fid) throws Exception {
+		
+		return financialAdvisorService.getFinancialAdvisor(fid);
+	}
+	
+	@PutMapping("/updateFA/{fid}")
+    public FinancialAdvisor updateFinancialAdvisor(@PathVariable long fid, @RequestBody FinancialAdvisor financialAdvisorDetails) throws Exception {
+		return financialAdvisorService.updateFinancialAdvisor(fid, financialAdvisorDetails);
+	
+	}
 }
--- Online_Investment_Banking/src/main/java/com/online_investment_banking/service/FinancialAdvisorService.java
+++ Online_Investment_Banking/src/main/java/com/online_investment_banking/service/FinancialAdvisorService.java
@@ -1,8 +1,8 @@
 package com.online_investment_banking.service;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.online_investment_banking.model.FinancialAdvisor;
@@ -18,4 +18,33 @@ public class FinancialAdvisorService {
 		return financialAdvisorRepo.findAll();
 	}
 
+	public FinancialAdvisor createFinancialAdvisor(FinancialAdvisor financialAdvisor) {
+		
+		return financialAdvisorRepo.save(financialAdvisor);
+	}
+	
+    public ResponseEntity<FinancialAdvisor> getFinancialAdvisor(long fid) throws Exception {
+		
+		FinancialAdvisor financialAdvisor = financialAdvisorRepo.findById(fid)
+				.orElseThrow(()->new Exception("Financial Advisor not found with ID: " + fid));
+		return ResponseEntity.ok(financialAdvisor);
+	}
+    
+    public FinancialAdvisor updateFinancialAdvisor(long fid, FinancialAdvisor financialAdvisorDetails) throws Exception
+    {
+    	FinancialAdvisor financialAdvisor = financialAdvisorRepo.findById(fid)
+				.orElseThrow(()->new Exception("Financial Advisor not found with ID: " + fid));
+    	
+    	financialAdvisor.setFirstName(financialAdvisorDetails.getFirstName());
+    	financialAdvisor.setLastName(financialAdvisorDetails.getLastName());
+    	financialAdvisor.setEmailId(financialAdvisorDetails.getEmailId());
+    	financialAdvisor.setMobileNumber(financialAdvisorDetails.getMobileNumber());
+    	financialAdvisor.setBranchNumber(financialAdvisorDetails.getBranchNumber());
+    	
+    	FinancialAdvisor updatedFinancialAdvisor = financialAdvisorRepo.save(financialAdvisor);	
+    	
+		return updatedFinancialAdvisor;
+    }
+    
+    
 }
